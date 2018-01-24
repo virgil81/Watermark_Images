9ac6c4ce1812c610224597650346288dbc53c3dc
virgil <virgil.peptu@gmail.com>
2018-01-24 17:26:54 +0200

    for 3 chanels

--- Watermark_Images/Watermark_Manager.cpp
+++ Watermark_Images/Watermark_Manager.cpp
@@ -8,8 +8,8 @@ Watermark_Manager::Watermark_Manager()
 
 Watermark_Manager::Watermark_Manager(std::string path,std::string new_file)
 {
-	_image = cv::imread(path, 0);
-	_hideimage = cv::imread(new_file, 0);
+	_image = cv::imread(path);
+	_hideimage = cv::imread(new_file);
 	cv::resize(_hideimage, _hideimage, _image.size());
 }
 
@@ -25,9 +25,14 @@ void Watermark_Manager::Watermark_start()
 		{
 			//uchar val = _image.at<uchar>(j, i);
 			//if (val != 0)
-			_result.at<uchar>(j, i) = _image.at<uchar>(j, i) >> 2;
+			_result.at<cv::Vec<uchar, 3> >(j, i).val[0] = _image.at<cv::Vec<uchar, 3> >(j, i).val[0] >> 2;
+			_result.at<cv::Vec<uchar, 3> >(j, i).val[1] = _image.at<cv::Vec<uchar, 3> >(j, i).val[1] >> 2;
+			_result.at<cv::Vec<uchar, 3> >(j, i).val[2] = _image.at<cv::Vec<uchar, 3> >(j, i).val[2] >> 2;
+
 			_result.at<uchar>(j, i) = _result.at<uchar>(j, i) << 2;
+
 			hiddenimage.at<uchar>(j, i) = _hideimage.at<uchar>(j, i) >> 6;
+
 			_result.at<uchar>(j, i) = _result.at<uchar>(j, i) | hiddenimage.at<uchar>(j, i);
 		}
 	}
@@ -42,10 +47,17 @@ void Watermark_Manager::Watermark_decrypt()
 	{
 		for (size_t j = 0; j < _result.rows; j++)
 		{
-			hiddenimage.at<uchar>(j, i) = _result.at<uchar>(j, i) << 6;
 			
+			hiddenimage.at<cv::Vec<uchar,3>>(j, i).val[0] = _result.at<cv::Vec<uchar,3>>(j, i).val[0] << 6;
+			hiddenimage.at<cv::Vec<uchar, 3>>(j, i).val[1] = _result.at<cv::Vec<uchar, 3>>(j, i).val[1] << 6;
+			hiddenimage.at<cv::Vec<uchar, 3>>(j, i).val[2] = _result.at<cv::Vec<uchar, 3>>(j, i).val[2] << 6;
+
+
 		}
 	}
+	cv::namedWindow("result", 0);
+	cv::imshow("result", hiddenimage);
+	cv::waitKey();
 }
 
 Watermark_Manager::~Watermark_Manager()
