2f6b96f8413a55b83af0017da445635e379d0b51
virgil <virgil.peptu@gmail.com>
2018-03-13 09:20:25 +0200

    Color encrypt+decrypt : visible & invisible

--- Watermark_Images/Watermark_Manager.cpp
+++ Watermark_Images/Watermark_Manager.cpp
@@ -6,7 +6,7 @@ Watermark_Manager::Watermark_Manager()
 {
 }
 
-Watermark_Manager::Watermark_Manager(std::string path,std::string new_file)
+Watermark_Manager::Watermark_Manager(std::string path, std::string new_file)
 {
 	_image = cv::imread(path);
 	_hideimage = cv::imread(new_file);
@@ -25,21 +25,33 @@ void Watermark_Manager::Watermark_start()
 		{
 			//uchar val = _image.at<uchar>(j, i);
 			//if (val != 0)
-			_result.at<cv::Vec<uchar, 3> >(j, i).val[0] = _image.at<cv::Vec<uchar, 3> >(j, i).val[0] >> 2;
-			_result.at<cv::Vec<uchar, 3> >(j, i).val[1] = _image.at<cv::Vec<uchar, 3> >(j, i).val[1] >> 2;
-			_result.at<cv::Vec<uchar, 3> >(j, i).val[2] = _image.at<cv::Vec<uchar, 3> >(j, i).val[2] >> 2;
+			_result.at<cv::Vec<uchar, 3> >(j, i).val[0] = _image.at<cv::Vec<uchar, 3> >(j, i).val[0] >> 6;
+			_result.at<cv::Vec<uchar, 3> >(j, i).val[1] = _image.at<cv::Vec<uchar, 3> >(j, i).val[1] >> 6;
+			_result.at<cv::Vec<uchar, 3> >(j, i).val[2] = _image.at<cv::Vec<uchar, 3> >(j, i).val[2] >> 6;
 
-			_result.at<uchar>(j, i) = _result.at<uchar>(j, i) << 2;
+			_result.at<cv::Vec<uchar, 3> >(j, i).val[0] = _result.at<cv::Vec<uchar, 3> >(j, i).val[0] << 6;
+			_result.at<cv::Vec<uchar, 3> >(j, i).val[1] = _result.at<cv::Vec<uchar, 3> >(j, i).val[1] << 6;
+			_result.at<cv::Vec<uchar, 3> >(j, i).val[2] = _result.at<cv::Vec<uchar, 3> >(j, i).val[2] << 6;
 
-			hiddenimage.at<uchar>(j, i) = _hideimage.at<uchar>(j, i) >> 6;
+			hiddenimage.at<cv::Vec<uchar, 3> >(j, i).val[0] = _hideimage.at<cv::Vec<uchar, 3> >(j, i).val[0] >> 2;
+			hiddenimage.at<cv::Vec<uchar, 3> >(j, i).val[1] = _hideimage.at<cv::Vec<uchar, 3> >(j, i).val[1] >> 2;
+			hiddenimage.at<cv::Vec<uchar, 3> >(j, i).val[2] = _hideimage.at<cv::Vec<uchar, 3> >(j, i).val[2] >> 2;
 
-			_result.at<uchar>(j, i) = _result.at<uchar>(j, i) | hiddenimage.at<uchar>(j, i);
+
+
+			_result.at<cv::Vec<uchar, 3> >(j, i).val[0] = _result.at<cv::Vec<uchar, 3> >(j, i).val[0] | hiddenimage.at<cv::Vec<uchar, 3> >(j, i).val[0];
+			_result.at<cv::Vec<uchar, 3> >(j, i).val[1] = _result.at<cv::Vec<uchar, 3> >(j, i).val[1] | hiddenimage.at<cv::Vec<uchar, 3> >(j, i).val[1];
+			_result.at<cv::Vec<uchar, 3> >(j, i).val[2] = _result.at<cv::Vec<uchar, 3> >(j, i).val[2] | hiddenimage.at<cv::Vec<uchar, 3> >(j, i).val[2];
 		}
 	}
+
+	cv::namedWindow("result1", 0);
+	cv::imshow("result1", _result);
+	cv::waitKey();
 }
 void Watermark_Manager::Watermark_decrypt()
 {
-	
+
 	cv::Mat hiddenimage = _result.clone();
 
 
@@ -47,16 +59,16 @@ void Watermark_Manager::Watermark_decrypt()
 	{
 		for (size_t j = 0; j < _result.rows; j++)
 		{
-			
-			hiddenimage.at<cv::Vec<uchar,3>>(j, i).val[0] = _result.at<cv::Vec<uchar,3>>(j, i).val[0] << 6;
-			hiddenimage.at<cv::Vec<uchar, 3>>(j, i).val[1] = _result.at<cv::Vec<uchar, 3>>(j, i).val[1] << 6;
-			hiddenimage.at<cv::Vec<uchar, 3>>(j, i).val[2] = _result.at<cv::Vec<uchar, 3>>(j, i).val[2] << 6;
+
+			hiddenimage.at<cv::Vec<uchar, 3>>(j, i).val[0] = _result.at<cv::Vec<uchar, 3>>(j, i).val[0] << 2;
+			hiddenimage.at<cv::Vec<uchar, 3>>(j, i).val[1] = _result.at<cv::Vec<uchar, 3>>(j, i).val[1] << 2;
+			hiddenimage.at<cv::Vec<uchar, 3>>(j, i).val[2] = _result.at<cv::Vec<uchar, 3>>(j, i).val[2] << 2;
 
 
 		}
 	}
-	cv::namedWindow("result", 0);
-	cv::imshow("result", hiddenimage);
+	cv::namedWindow("result2", 0);
+	cv::imshow("result2", hiddenimage);
 	cv::waitKey();
 }
 
